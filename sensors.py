ef6c329b6b717cbcc58e809d74aff8c165deb0f2
Daan De Wilde <71894656+DeWildeDaan@users.noreply.github.com>
2021-10-21 11:39:00 +0200

    python zonder api calls

--- sensors.py
+++ sensors.py
@@ -4,13 +4,28 @@ import urllib.request
 import sys
 import random
 
+def on_connect(client, userdata, flags, rc):
+    pass
+    #print("Connected with result code " +str(rc))
+
+def on_message(client, userdata, msg):
+    # print(msg.topic + " " + str(msg.payload))
+    try:
+        data = json.loads(msg.payload.decode("utf-8"))
+        print(data)
+    except:
+        print("An error has occured!")
  
 def menu(keuze):
-
- 
+    aantal = 0
     if keuze == 9:
         sys.exit()
-
+    elif keuze == 1:
+        aantal = random.randrange(100)
+        print(f"sensor: water \taantal: {aantal} \tprijs:  \temail: {email}")
+    elif keuze == 2:
+        aantal = random.randrange(100)
+        print(f"sensor: water \taantal: {aantal} \tprijs:  \temail: {email}")
  
 
 def run():
@@ -28,7 +43,8 @@ if __name__ == '__main__':
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect("", 1883, 60)
-    client.subscribe("")
+    client.connect("13.81.105.139", 1883, 60)
+    client.subscribe("/daandewilde", qos=0)
     client.loop_start()
+    email = input("Geef je e-mail in om ver6der te gaan:\n")
     run()
